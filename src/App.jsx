df20c22c8cedbdbb410b392852ebcafdec841929
Mohammad Hamo <89643788+MohammadHamo912@users.noreply.github.com>
2025-03-14 00:59:44 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Heading from "./Heading";
+import Paragraph from "./Paragraph";
+
+function App() {
+  return (
+    <div>
+      <Heading />
+      <Paragraph />
+    </div>
+  );
+}
+export default App;
